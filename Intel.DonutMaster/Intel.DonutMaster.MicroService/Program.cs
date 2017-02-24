f19ec872138f3aa833cce9655a5838ce9c67a396
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-02-24 13:59:54 +0100

    Utworzenie mikro usługi - selfhosting

---
+++ Intel.DonutMaster/Intel.DonutMaster.MicroService/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intel.DonutMaster.MicroService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("DonutMaster Started!");
+
+            // TODO
+
+            string address = "http://localhost:5010";
+
+            using (var webapp = WebApp.Start<Startup>(address))
+            {
+                Console.WriteLine("Press any key to exit.");
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
