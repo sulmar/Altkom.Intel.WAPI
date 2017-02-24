57c3e38b4ccef7b091cc1d80cac9a04437bdb29c
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-02-24 15:31:02 +0100

    Utworzenie klienta RestApi

---
+++ Intel.DonutMaster/Intel.DonutMaster.ConsoleClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using Intel.DonutMaster.Interfaces;
+using Intel.DonutMaster.RestApiServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intel.DonutMaster.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task.Run(()=>GetProductsTest());
+
+            Console.ReadKey();
+        }
+
+        private static async Task GetProductsTest()
+        {
+            IProductsService productsService = new RestApiProductsService();
+
+            var products = await productsService.GetAsync();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Name);
+            }
+        }
+    }
+}
---
+++ Intel.DonutMaster/Intel.DonutMaster.RestApiServices/RestApiProductsService.cs
@@ -0,0 +1,67 @@
+﻿using Intel.DonutMaster.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intel.DonutMaster.Models;
+using System.Net.Http;
+
+namespace Intel.DonutMaster.RestApiServices
+{
+    public class RestApiProductsService : IProductsService
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(string symbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IList<Product>> GetAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:52765");
+                client.DefaultRequestHeaders.Add("Secret-Key", "12345");
+
+                var response = await client.GetStringAsync("api/Products");
+
+                throw new NotImplementedException();
+            }
+        }
+
+        public Recipe GetByProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<RecipeStep> GetRecipeSteps(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
